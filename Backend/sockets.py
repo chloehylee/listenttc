dc78418eab80be9bad7a561890c9a14d10337ad6
Ethan-Lem05 <88153797+Ethan-Lem05@users.noreply.github.com>
2023-11-04 17:05:23 -0400

    Update sockets.py

---
+++ Backend/sockets.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+SOCKET_IP = "127.0.0.1"
+SOCKET_PORT = 5000
+server = None
+connected_clients = []
+
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SOCKET_IP, SOCKET_PORT))
+    server.listen(5)
+    print(f"WebSocket server listening on {SOCKET_IP
+    }:{SOCKET_PORT}")
+    return (SOCKET_IP,SOCKET_PORT);
+
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        for client in connected_clients:
+            data = 0;
+            # process the data for each client
+            client.send(data)
+
+def main():
+    #connected_clients
+    while True:
+        client, addr = server.accept()
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        connected_clients.append(client)
+
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(client,))
+        client_thread.start()
