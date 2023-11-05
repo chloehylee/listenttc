831bc1133266fd1a7229412f330424286d81a691
celinac23 <Celina.y.125@gmail.com>
2023-11-05 00:45:51 -0400

    Reusing Whisper now

--- Backend/speech_to_text.py
+++ Backend/speech_to_text.py
@@ -1,40 +1,45 @@
+# from google.cloud import speech
 
-from google.cloud import speech
+# import os
+# from dotenv import load_dotenv
 
-def run_quickstart() -> speech.RecognizeResponse:
-    # Instantiates a client
-    client = speech.SpeechClient()
+# load_dotenv()
 
-    # The name of the audio file to transcribe
-    gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+# Google_Key = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-    audio = speech.RecognitionAudio(uri=gcs_uri)
+# def run_quickstart() -> speech.RecognizeResponse:
+#     # Instantiates a client
+#     client = speech.SpeechClient()
 
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
+#     # The name of the audio file to transcribe
+#     gcs_uri = "cloud-samples-data/speech/brooklyn_bridge.wav"
 
-    # Detects speech in the audio file
-    response = client.recognize(config=config, audio=audio)
+#     audio = speech.RecognitionAudio(uri=gcs_uri)
 
-    for result in response.results:
-        print(f"Transcript: {result.alternatives[0].transcript}")
-        
-        
-        
-# import whisper
-# model = whisper.load_model("base")
-# result = model.transcribe("audio.file")  #need to change later to file name
+#     config = speech.RecognitionConfig(
+#         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+#         sample_rate_hertz=16000,
+#         language_code="en-US",
+#     )
 
-# transcribed_text = result["text"]
+#     # Detects speech in the audio file
+#     response = client.recognize(config=config, audio=audio)
 
-# # print(transcribed_text)
+#     for result in response.results:
+#         print(f"Transcript: {result.alternatives[0].transcript}")
+
+# run_quickstart()
+import whisper
+model = whisper.load_model("base")
+result = model.transcribe("c:/Users/celin/Downloads/testing1.m4a") 
+
+transcribed_text = result["text"]
+
+print(transcribed_text)
 
 # with open("transcription.text" "w") as text_file:
 #     text_file.write(result["text"])
-# # a file in Backend called transcription.text will be created with the transcribed text inside
+# a file in Backend called transcription.text will be created with the transcribed text inside
 
     
     
